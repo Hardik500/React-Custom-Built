32ff4c655e4d6f3912b7cbb0ca266787daeb096a
Hardik <hardikkhandelwal500@gmail.com>
2020-07-06 10:03:32 +0530

    [add] production webpack build && [changed] font load style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./app.jsx";
+import WebFont from "webfontloader";
+
+//Note: Doesn't prevent CLS
+// WebFont.load({
+//   google: {
+//     families: ["Titillium Web:300,400,700", "sans-serif"],
+//   },
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-      <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
---
+++ webpack.prod.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader"],
+        }),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      hash: true,
+      filename: "index.html", //target html
+      template: "./src/index.html", //source html
+    }),
+    new ExtractTextPlugin({ filename: "css/style.css" }),
+  ],
+};
